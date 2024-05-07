d1f311f747344c43354b5ceb75e2095c2979a592
dairen <djean032@gmail.com>
2024-05-06 23:07:49 -0400

    refactor and started scan function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,55 @@
 mod stepper;
-use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::peripherals::Peripherals;
 
-
-
-
-fn main() ->  anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-    
+
     let per = Peripherals::take()?;
 
-    let mut grating_motor = stepper::Stepper::new(per.pins.gpio1, per.pins.gpio2, per.pins.gpio3, per.pins.gpio4);
+    let mut grating_motor = stepper::Stepper::new(
+        per.pins.gpio1,
+        per.pins.gpio2,
+        per.pins.gpio3,
+        per.pins.gpio4,
+    );
 
     loop {
-        grating_motor.in2.set_high()?;
-        FreeRtos::delay_ms(1000);
-        grating_motor.in2.set_low()?;
-        FreeRtos::delay_ms(1000);
+        grating_motor.step_forward()?;
+        grating_motor.step_backward()?;
     }
+}
 
+fn scan(
+    current_position: i32,
+    start_wavelength: i32,
+    end_wavelength: i32,
+    mut motor: stepper::Stepper,
+) -> anyhow::Result<()> {
+    if current_position == start_wavelength {
+        let difference = end_wavelength - start_wavelength;
+        // TODO
+        let mut steps = difference / motor.step_size;
+
+        while number != 0 {
+            motor.step_forward()?;
+            number += 1;
+        }
+    } else {
+        let mut number = current_position - start_wavelength;
+        if number > 0 {
+            while number != start_wavelength {
+                motor.step_backward()?;
+                number -= 1;
+            }
+
+        } else {
+            while number != start_wavelength {
+                motor.step_forward()?;
+                number += 1;
+            }
+        }
+    }
+    Ok(())
 }
--- src/stepper.rs
+++ src/stepper.rs
@@ -1,14 +1,11 @@
-use core::f32;
-use core::i32;
-use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
-
+use esp_idf_hal::delay::FreeRtos;
+//TODO implement step_size
 pub struct Stepper {
     pub in1: PinDriver<'static, AnyIOPin, Output>,
     pub in2: PinDriver<'static, AnyIOPin, Output>,
     pub in3: PinDriver<'static, AnyIOPin, Output>,
     pub in4: PinDriver<'static, AnyIOPin, Output>,
-    pub step_size: f32,
 }
 
 impl Stepper {
@@ -17,7 +14,6 @@ impl Stepper {
         pin2: impl IOPin + 'static,
         pin3: impl IOPin + 'static,
         pin4: impl IOPin + 'static,
-        step_size: f32,
     ) -> Stepper {
         let pin1 = pin1.downgrade();
         let in1 = PinDriver::output(pin1).unwrap();
@@ -27,49 +23,22 @@ impl Stepper {
         let in3 = PinDriver::output(pin3).unwrap();
         let pin4 = pin4.downgrade();
         let in4 = PinDriver::output(pin4).unwrap();
-        Stepper {
-            in1,
-            in2,
-            in3,
-            in4,
-            step_size,
-        }
+        Stepper { in1, in2, in3, in4 }
     }
-    pub fn man_step(&self, wavelength_requested: f32, current_position: f32) {
-        let distance = current_position - wavelength_requested;
-        let mut direction = true;
-        if distance < 0. {
-            direction = false
-        } else {
-        }
-        let steps = (distance / self.step_size) as i32;
-        if steps > 0 {
-            for i in [1..steps + 1] {
-                self.in1.set_high();
-                FreeRtos::delay_ms(300);
-                self.in1.set_low();
-            }
-        } else {
-            let abs_steps = steps.abs();
-            for i in [1..steps + 1] {
-                self.in3.set_high();
-                FreeRtos::delay_ms(300);
-                self.in3.set_low();
-            }
-        }
+
+    pub fn step_forward (&mut self) -> anyhow::Result<()> {
+        self.in1.set_high()?;
+        FreeRtos::delay_ms(300);
+        self.in1.set_low()?;
+        FreeRtos::delay_ms(300);
+        Ok(())
     }
-    /*
-        fn get_pin1(self) -> PinDriver<'static, AnyIOPin, Output> {
-            self.in1
-        }
-        fn get_pin2(self) -> PinDriver<'static, AnyIOPin, Output> {
-            self.in2
-        }
-        fn get_pin3(self) -> PinDriver<'static, AnyIOPin, Output> {
-            self.in3
-        }
-        fn get_pin4(self) -> PinDriver<'static, AnyIOPin, Output> {
-            self.in4
-        }
-    */
+    
+    pub fn step_backward (&mut self) -> anyhow::Result<()> {
+        self.in3.set_high()?;
+        FreeRtos::delay_ms(300);
+        self.in3.set_low()?;
+        FreeRtos::delay_ms(300);
+        Ok(())
+    }
 }
